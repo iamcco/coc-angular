29a4ce6e170e279dc68967b49b292c0ab5e7134a
iamcco <ooiss@qq.com>
2019-10-12 11:09:10 +0800

    update

--- client/index.ts
+++ client/index.ts
@@ -1,53 +1,60 @@
-// import path from 'path';
-import {
-  ExtensionContext,
-  LanguageClient,
-  LanguageClientOptions,
-  ServerOptions,
-  TransportKind,
-  RevealOutputChannelOn,
-  workspace,
-  services
-} from 'coc.nvim';
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
 
-export function activate(context: ExtensionContext) {
-  const config = workspace.getConfiguration('angular')
-  const isEnableDebug = config.get<boolean>('angular.debug')
-  // The server is implemented in node
-  const serverModule = require.resolve('angular-lsp-service')
+import {tmpdir} from 'os';
+import * as path from 'path';
+import * as coc from 'coc.nvim';
+import * as lsp from 'vscode-languageclient';
 
-  // FIXME: coc.nvim do not have logPath
+import {registerCommands} from './commands';
+import {projectLoadingNotification} from './protocol';
+
+export function activate(context: coc.ExtensionContext) {
   // Log file does not yet exist on disk. It is up to the server to create the
   // file.
-  // const logFile = path.join(context.logPath, 'nglangsvc.log');
-
+  const logFile = path.join(tmpdir(), 'nglangsvc.log');
+  const ngProbeLocations = getProbeLocations('angular.ngdk', context.asAbsolutePath('server'));
+  const tsProbeLocations = getProbeLocations('typescript.tsdk', context.extensionPath);
   // If the extension is launched in debug mode then the debug server options are used
   // Otherwise the run options are used
-  let serverOptions: ServerOptions = {
-    run : {
-      module: serverModule,
-      transport: TransportKind.ipc,
-      // args: [
-        // '--logFile', logFile,
+  const serverOptions: lsp.ServerOptions = {
+    run: {
+      module: context.asAbsolutePath(path.join('server')),
+      transport: lsp.TransportKind.ipc,
+      args: [
+        '--logFile',
+        logFile,
         // TODO: Might want to turn off logging completely.
-      // ],
+        '--ngProbeLocations',
+        ngProbeLocations.join(','),
+        '--tsProbeLocations',
+        tsProbeLocations.join(','),
+      ],
       options: {
         env: {
           // Force TypeScript to use the non-polling version of the file watchers.
           TSC_NONPOLLING_WATCHER: true,
         },
-        execArgv: [
-          '--max_old_space_size=4096'
-        ]
       },
     },
     debug: {
-      module: serverModule,
-      transport: TransportKind.ipc,
-      // args: [
-        // '--logFile', logFile,
-        // '--logVerbosity', 'verbose',
-      // ],
+      module: context.asAbsolutePath(path.join('server', 'out', 'server.js')),
+      transport: lsp.TransportKind.ipc,
+      args: [
+        '--logFile',
+        logFile,
+        '--logVerbosity',
+        'verbose',
+        '--ngProbeLocations',
+        ngProbeLocations.join(','),
+        '--tsProbeLocations',
+        tsProbeLocations.join(','),
+      ],
       options: {
         env: {
           // Force TypeScript to use the non-polling version of the file watchers.
@@ -56,16 +63,16 @@ export function activate(context: ExtensionContext) {
         },
         execArgv: [
           // do not lazily evaluate the code so all breakpoints are respected
-          '--max_old_space_size=4096',
           '--nolazy',
-          "--inspect=6009",
+          // If debugging port is changed, update .vscode/launch.json as well
+          '--inspect=6009',
         ]
       },
     },
-  }
+  };
 
   // Options to control the language client
-  const clientOptions: LanguageClientOptions = {
+  const clientOptions: lsp.LanguageClientOptions = {
     // Register the server for Angular templates and TypeScript documents
     documentSelector: [
       // scheme: 'file' means listen to changes to files on disk only
@@ -77,24 +84,66 @@ export function activate(context: ExtensionContext) {
     synchronize: {
       fileEvents: [
         // Notify the server about file changes to tsconfig.json contained in the workspace
-        workspace.createFileSystemWatcher('**/tsconfig.json'),
+        coc.workspace.createFileSystemWatcher('**/tsconfig.json'),
       ]
     },
 
     // Don't let our output console pop open
-    revealOutputChannelOn: RevealOutputChannelOn.Never
-  }
+    revealOutputChannelOn: lsp.RevealOutputChannelOn.Never
+  };
 
   // Create the language client and start the client.
-  let client = new LanguageClient(
-    'angularls',
-    'Angular Language Service',
-    serverOptions,
-    clientOptions,
-    isEnableDebug
-  );
+  const forceDebug = !!process.env['NG_DEBUG'];
+  const client =
+      new lsp.LanguageClient('Angular Language Service', serverOptions, clientOptions, forceDebug);
 
   // Push the disposable to the context's subscriptions so that the
   // client can be deactivated on extension deactivation
-  context.subscriptions.push(services.registLanguageClient(client));
+  context.subscriptions.push(
+      ...registerCommands(client),
+      client.start(),
+  );
+
+  client.onReady().then(() => {
+    const projectLoadingTasks = new Map<string, {resolve: () => void}>();
+
+    client.onNotification(projectLoadingNotification.start, (projectName: string) => {
+      const statusBar = coc.workspace.createStatusBarItem(0, { progress: true })
+      statusBar.text = 'Initializing Angular language features'
+      projectLoadingTasks.set(
+        projectName,
+        {
+          resolve: () => {
+            statusBar.isProgress = false
+            statusBar.hide()
+            statusBar.dispose()
+          }
+      })
+    });
+
+    client.onNotification(projectLoadingNotification.finish, (projectName: string) => {
+      const task = projectLoadingTasks.get(projectName);
+      if (task) {
+        task.resolve();
+        projectLoadingTasks.delete(projectName);
+      }
+    });
+  });
+}
+
+function getProbeLocations(configName: string, bundled: string): string[] {
+  const locations = [];
+  // Always use config value if it's specified
+  const configValue = coc.workspace.getConfiguration().get(configName);
+  if (configValue) {
+    locations.push(configValue as string);
+  }
+  // If not, look in workspaces currently open
+  const workspaceFolders = coc.workspace.workspaceFolders || [];
+  for (const folder of workspaceFolders) {
+    locations.push(coc.Uri.parse(folder.uri).fsPath);
+  }
+  // If all else fails, load the bundled version
+  locations.push(bundled);
+  return locations;
 }
